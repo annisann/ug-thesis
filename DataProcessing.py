7adc066c8d424e4e98b2b77fcc0a903f1f485b9a
annisann <ennoza@student.ub.ac.id>
2022-04-08 21:10:08 +0700

    add splitData - splitting data into n utterances

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,5 +1,7 @@
 import glob
+import os
 import re
+
 
 class DataProcessing:
     """
@@ -7,8 +9,16 @@ class DataProcessing:
     """
 
     def __init__(self):
+        """
+        data = [[Utterance1, Utterance2, ..., UtteranceN],
+                [Utterance1, Utterance2, ..., UtteranceN],
+                ...
+                [Utterance1, Utterance2, ..., UtteranceN]
+                ]
+        """
         self.path = 'dataset/Session*/dialog/transcriptions/*'
-        self.data = []
+        self.data = list()
+        self.fixedData = list()
 
     def loadData(self):
         path = self.path
@@ -41,7 +51,6 @@ class DataProcessing:
     def fixUtterance(self, conv):
         counter = 1
         utt_per_speaker = []
-        utterances = []
 
         # COUNT DUPLICATE CONSECUTIVE UTTERANCES
         for i in range(len(conv) - 1):
@@ -57,23 +66,58 @@ class DataProcessing:
 
         # MERGE DUPLICATE CONS. UTTERANCES INTO ONE
         index = 0
+        utterances = []
         for n in utt_per_speaker:
             index += n
 
             decode = self.getSpeaker(conv[index - 1]) + " : " + ' '.join(
                 [self.getUtterance(c) for c in conv[index - n:index]])
             utterances.append(decode)
+        self.fixedData.append(utterances)
 
-        return utterances
+    def splitData(self, n):
+        PATH = os.getcwd() + '/n_' + str(n)
+        if not os.path.exists(PATH):
+            os.makedirs(PATH)
+
+        idx = [i for i in range(0, len(self.fixedData), n)]
+        i = 0
+        while i < len(self.fixedData):
+            counter = 0
+            while counter < len(idx)-1:
+                with open('{}/script{}_{}'.format(PATH, str(i), str(counter)), 'w') as file:
+                    if len(self.fixedData[i][idx[counter]:idx[counter+1]]) == 3: # valuenya 3
+                        for u in self.fixedData[i][idx[counter]:idx[counter+1]]:
+                            file.write('%s\n' % u)
+                            # print('{} idx[{}]:idx[{}]'.format(i, idx[counter], idx[counter+1]))
+                    else:
+                        break
+                counter += 1
+            i += 1
+
+        # if len(self.fixedData[0][idx[0]:idx[1]]) == 3:
+        #     print(1)
+        # print((self.fixedData[0][idx[0]:idx[1]]))
 
 
 if __name__ == '__main__':
     dp = DataProcessing()
     dp.loadData()
     data = dp.data
+    data = [dp.fixUtterance(d) for d in data]
+    data = dp.fixedData
+    dp.splitData(3) # BELOM CEK KALO N NYA > 3
 
-    _ = []
-    for d in data:
-        _.append(len(d) % 3)
-    print((_))
-    print(_.count(0))
+    a = 0
+    b = len(data)  # len data
+    c = 3  # berapa banyak mau displit
+    #
+    # # data[for i in range len(data)] [a:c?]
+    # print(data[0][0:3])
+    # print(data[0][3:6])
+    # print()
+    # print(data[1][0:3])
+    # print()
+
+    # print('len_data: ', len(data))
+    # print('range_len_data', range(len(data)))
