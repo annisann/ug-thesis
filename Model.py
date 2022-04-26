fb0e6118c2edb77b9b3940a4d31d3e0cf0bd2d7f
annisann <ennoza@student.ub.ac.id>
2022-04-26 14:13:19 +0700

    adjust

--- Model.py
+++ Model.py
@@ -8,10 +8,65 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
 
 
+class ConvEmoRecogDataset(Dataset):
+
+    def load_dataset(self):
+        folder = f'n_{self.utterance_num}'
+        scripts = os.listdir(folder)
+        scripts = [f'{folder}/{script}' for script in scripts if os.path.getsize(f'{folder}/{script}') != 0]
+
+        # SPLIT DATA, hasilnya adalah list of dir ['n_2/..', ..., ...]
+        # ratio 8:1:1
+        trainData, testData = train_test_split(scripts, train_size=0.8, random_state=10)
+        testData, valData = train_test_split(testData, test_size=0.5, random_state=8)
+
+        traindf, valdf, testdf = [], [], []
+
+        for data in [trainData, valData, testData]:
+            for script in data:
+                f = open(script).read()
+                f = ast.literal_eval(f)
+                df = pd.DataFrame(f)
+                if data == trainData:
+                    traindf.append(df)
+                elif data == valData:
+                    valdf.append(df)
+                elif data == testData:
+                    testdf.append(df)
+
+        traindf = pd.concat(traindf, ignore_index=True)
+        valdf = pd.concat(valdf, ignore_index=True)
+        testdf = pd.concat(testdf, ignore_index=True)
+        return traindf, valdf, testdf
+
+    def __init__(self, utterance_num):
+        self.utterance_num = utterance_num
+        self.train, self.val, self.test = self.load_dataset()
+
+    def __len__(self):
+        return len(self.train) / self.utterance_num, \
+               len(self.val) / self.utterance_num, \
+               len(self.test) / self.utterance_num
+
+    def get_data(self, data, index):
+        # index = file ke-n
+        index_1 = index * self.utterance_num
+        index_2 = index_1 + self.utterance_num
+        data = data.iloc[index_1:index_2]
+        token, v, a, d = data.token, data.v, data.a, data.d
+
+        return np.array(token), np.array(v), np.array(a), np.array(d)
+
+    def max_token_len(self):
+        return max(list(map(lambda token: len(token), self.train.token))), \
+               max(list(map(lambda token: len(token), self.val.token))), \
+               max(list(map(lambda token: len(token), self.test.token)))
+
+
 class PretrainedEmbeddings(object):
     """ A wrapper around pre-trained word vectors and their use """
 
-    def load_from_file(self, dim): # vocab glove
+    def load_from_file(self, dim):  # vocab glove
         """Instantiate from pre-trained vector file.
 
         Vector file should be of the format:
@@ -24,7 +79,7 @@ class PretrainedEmbeddings(object):
             instance of PretrainedEmbeddigns
         """
         self.dim = dim
-        self.word2index = {} # vocabulary
+        self.word2index = {}  # vocabulary
         self.embeddings = []
 
         self.word2index['<PAD>'] = 0
@@ -77,65 +132,12 @@ class PretrainedEmbeddings(object):
         words += ['<PAD>'] * (max_len - len(words))
 
         for i, word in enumerate(words):
-            if word in self.word2idx: # if word is in vocabulary
+            if word in self.word2idx:  # if word is in vocabulary
                 self.final_embeddings[i, :] = self.embeddings[self.word2idx[word]]
             else:
                 embedding_i = torch.ones(1, self.embedding_size)
-                torch.nn.init.xavier_uniform_(embedding_i) # random value, as np.random
+                torch.nn.init.xavier_uniform_(embedding_i)  # random value, as np.random
                 self.final_embeddings[i, :] = embedding_i
         return self.final_embeddings
 
 
-class ConvEmoRecogDataset(Dataset):
-
-    def load_dataset(self):
-        folder = f'n_{self.utterance_num}'
-        scripts = os.listdir(folder)
-        scripts = [f'{folder}/{script}' for script in scripts if os.path.getsize(f'{folder}/{script}') != 0]
-
-        # SPLIT DATA, hasilnya adalah list of dir ['n_2/..', ..., ...]
-        # ratio 8:1:1
-        trainData, testData = train_test_split(scripts, train_size=0.8, random_state=10)
-        testData, valData = train_test_split(testData, test_size=0.5, random_state=8)
-
-        traindf, valdf, testdf = [], [], []
-
-        for data in [trainData, valData, testData]:
-            for script in data:
-                f = open(script).read()
-                f = ast.literal_eval(f)
-                df = pd.DataFrame(f)
-                if data == trainData:
-                    traindf.append(df)
-                elif data == valData:
-                    valdf.append(df)
-                elif data == testData:
-                    testdf.append(df)
-
-        traindf= pd.concat(traindf, ignore_index=True)
-        valdf = pd.concat(valdf, ignore_index=True)
-        testdf = pd.concat(testdf, ignore_index=True)
-        return traindf, valdf, testdf
-
-    def __init__(self, utterance_num):
-        self.utterance_num = utterance_num
-        self.traindata, self.valdata, self.testdata = self.load_dataset()
-
-    def __len__(self):
-        return len(self.traindata)/self.utterance_num,\
-               len(self.valdata)/self.utterance_num,\
-               len(self.testdata)/self.utterance_num
-
-    def __getdata__(self, data, index):
-        # index = file ke-n
-        index_1 = index * self.utterance_num
-        index_2 = index_1 + self.utterance_num
-        data = data.iloc[index_1:index_2]
-        token, v, a, d = data.token, data.v, data.a, data.d
-
-        return np.array(token), np.array(v), np.array(a), np.array(d)
-
-    def max_token_len(self):
-        return max(list(map(lambda token: len(token), self.traindata.token))),\
-               max(list(map(lambda token: len(token), self.valdata.token))),\
-               max(list(map(lambda token: len(token), self.testdata.token)))
