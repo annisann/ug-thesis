7d72b6b1a1ddccbaca02941df74727042aca7649
annisann <ennoza@student.ub.ac.id>
2022-04-27 22:37:42 +0700

    change index :))))))

--- Model.py
+++ Model.py
@@ -8,6 +8,84 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
 
 
+class PretrainedEmbeddings(object):
+    """ A wrapper around pre-trained word vectors and their use """
+
+    def load_from_file(self, dim):  # vocab glove
+        """Instantiate from pre-trained vector file.
+
+        Vector file should be of the format:
+            word0 x0_0 x0_1 x0_2 x0_3 ... x0_N
+            word1 x1_0 x1_1 x1_2 x1_3 ... x1_N
+
+        Args:
+            dim (str): dimension of GloVe
+        Returns:
+            instance of PretrainedEmbeddigns
+        """
+        UNKNOWN_TOKEN = '<UNK>'
+        PADDING_TOKEN = '<PAD>'
+
+        self.dim = dim
+        self.word2index = {UNKNOWN_TOKEN: 0, PADDING_TOKEN: 1}  # JANGAN PAKE DICT
+        self.words = [UNKNOWN_TOKEN, PADDING_TOKEN]
+        self.embeddings = np.random.uniform(-0.25, 0.25, (2,50)).tolist()
+
+        embedding_file = 'glove.6B.{}d.txt'.format(self.dim)
+
+        with open(embedding_file, encoding="utf8") as fp:
+            for line in fp.readlines():
+                line = line.split()
+                word = line[0]
+                self.words.append(word)
+                vec = [float(x) for x in line[1:]]
+                self.embeddings.append(vec)
+                self.word2index[word] = len(self.word2index)
+
+        return self.words, np.array(self.embeddings), self.word2index
+
+    def get_embedding(self, word):
+        """
+        Args:
+            word (str)
+        Returns
+            an embedding (numpy.ndarray)
+        """
+        return self.embeddings[self.word2index[word]]
+
+    def get_index(self, index):
+        index2word = {idx: token for token, idx in self.word2index.items()}
+
+        if index not in index2word:
+            raise KeyError(f"Index {index} is not in vocabulary.")
+        return index2word[index]
+
+    def __len__(self):
+        return len(self.word2index)
+
+    # TODO: BIKIN BUAT DI DATA BUKAN CLASS EMBEDDINGS
+    def make_embedding_matrix(self, words, max_len):
+        """
+        padding and make embedding vectors
+        :param words: list of tokens
+        :return:
+        """
+        self.word2index, self.embeddings = self.load_from_file(self.dim)
+        self.embedding_size = self.embeddings.shape[1]
+        self.final_embeddings = np.zeros((len(words), self.embedding_size))
+
+        # PADDING
+        words += ['<PAD>'] * (max_len - len(words))
+
+        for i, word in enumerate(words):
+            if word in self.word2idx:  # if word is in vocabulary
+                self.final_embeddings[i, :] = self.embeddings[self.word2idx[word]]
+            else:
+                embedding_i = torch.ones(1, self.embedding_size)
+                torch.nn.init.xavier_uniform_(embedding_i)  # random value, as np.random
+                self.final_embeddings[i, :] = embedding_i
+        return self.final_embeddings
+
 class ConvEmoRecogDataset(Dataset):
 
     def load_dataset(self):
@@ -63,79 +141,36 @@ class ConvEmoRecogDataset(Dataset):
                max(list(map(lambda token: len(token), self.test.token)))
 
 
-class PretrainedEmbeddings(object):
-    """ A wrapper around pre-trained word vectors and their use """
+class Vocab:
+    # special token
+    UNKNOWN_TOKEN = '<UNK>'
+    PAD_TOKEN = '<PAD>'
 
-    def load_from_file(self, dim):  # vocab glove
-        """Instantiate from pre-trained vector file.
+    SPECIAL_TOKENS = {UNKNOWN_TOKEN,
+                      PAD_TOKEN}
 
-        Vector file should be of the format:
-            word0 x0_0 x0_1 x0_2 x0_3 ... x0_N
-            word1 x1_0 x1_1 x1_2 x1_3 ... x1_N
+    def __init__(self, min_freq=0):
+        # min_freq kepake gak?
+        self.word2index = {}
+        self.index2word = {}
+        self.word_freq = {}
+        self.min_freq = min_freq
 
-        Args:
-            dim (str): dimension of GloVe
-        Returns:
-            instance of PretrainedEmbeddigns
-        """
-        UNKNOWN_TOKEN = '<UNK>'
-        PADDING_TOKEN = '<PAD>'
-        self.dim = dim
-        self.word2index = {UNKNOWN_TOKEN: 0, PADDING_TOKEN: 1}  # JANGAN PAKE DICT
-        self.words = [UNKNOWN_TOKEN, PADDING_TOKEN]
-        self.embeddings = np.random.uniform(-0.25, 0.25, (2,50)).tolist()
+        self.add(UNKNOWN_TOKEN)
+        self.add(PAD_TOKEN)
 
-        embedding_file = 'glove.6B.{}d.txt'.format(self.dim)
+    def add(self, word, frequency=1):
+        if word in self.word2index:
+            self.word_freq[word] += frequency
+        else:
+            word_index = len(self._word2id)
+            self.word2index[word] = word_index
+            self.index2word[word_index] = word
+            self.word_freq[word] = frequency
 
-        with open(embedding_file, encoding="utf8") as fp:
-            for line in fp.readlines():
-                line = line.split()
-                word = line[0]
-                self.words.append(word)
-                vec = [float(x) for x in line[1:]]
-                self.embeddings.append(vec)
-                self.word2index[word] = len(self.word2index)
 
-        return self.words, np.array(self.embeddings), self.word2index
+class UtteranceEncoder:
+    # vektor utterance 1; label: v, a, d
+    # vektor utterance 2; label: v, a, d
 
-    def get_embedding(self, word):
-        """
-        Args:
-            word (str)
-        Returns
-            an embedding (numpy.ndarray)
-        """
-        return self.embeddings[self.word2index[word]]
-
-    def get_index(self, index):
-        self.index2word = {idx: token for token, idx in self.word2index.items()}
-
-        if index not in self.index2word:
-            raise KeyError(f"Index {index} is not in vocabulary.")
-        return self.index2word[index]
-
-    def __len__(self):
-        return len(self.word2index)
-
-    def make_embedding_matrix(self, words, max_len):
-        """
-        padding and make embedding vectors
-        :param words: list of tokens
-        :return:
-        """
-
-        self.word2index, self.embeddings = self.load_from_file(self.dim)
-        self.embedding_size = self.embeddings.shape[1]
-        self.final_embeddings = np.zeros((len(words), self.embedding_size))
-
-        # PADDING
-        words += ['<PAD>'] * (max_len - len(words))
-
-        for i, word in enumerate(words):
-            if word in self.word2idx:  # if word is in vocabulary
-                self.final_embeddings[i, :] = self.embeddings[self.word2idx[word]]
-            else:
-                embedding_i = torch.ones(1, self.embedding_size)
-                torch.nn.init.xavier_uniform_(embedding_i)  # random value, as np.random
-                self.final_embeddings[i, :] = embedding_i
-        return self.final_embeddings
+    pass
