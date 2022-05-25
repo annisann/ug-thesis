67948c9fa3f63dd04b3ab3719cb0de6684827651
annisann <ennoza@student.ub.ac.id>
2022-05-25 20:08:18 +0700

    add docstrings

--- Preprocessing.py
+++ Preprocessing.py
@@ -11,6 +11,7 @@ class Preprocessing:
         self.gloveFile = 'glove.6B.300d.txt'
         self.w2vFile = self.gloveFile + '.word2vec'
 
+        # Create word2vec file from GloVe
         if self.w2vFile not in os.listdir():
             print("Create W2V from GloVe!")
             glove2word2vec(self.gloveFile, self.w2vFile)
@@ -18,19 +19,63 @@ class Preprocessing:
             print("Found W2V file!")
             kv = KeyedVectors.load_word2vec_format(self.w2vFile, binary=False)
 
-        # load pycontractions
+        # Load pycontractions
         print('Loading KV Model . . .')
         self.PYCONTRACTIONS = Contractions(kv_model=kv)
 
-    def expand(self, utterance):
+    def expand(self, utterances):
+        """
+        Expanding contractions.
+            e.g. "He loves everything I've done to him.
+                  For example, my cooking suited his taste.
+                  Bet he'll miss me a lot?"
+                 -> "He loves everything I have done to him.
+                     For example, my cooking suited his taste.
+                     Bet he will miss me a lot?"
+        :param utterance: List
+        :return: List
+        """
         print('0_0 Expandiiiiiiing 0____0')
-        return list(self.PYCONTRACTIONS.expand_texts(utterance, precise=True))
+        return list(self.PYCONTRACTIONS.expand_texts(utterances, precise=True))
 
     def casefolding(self, utterance):
+        """
+        Turn a string into lowercase.
+            e.g. "He loves everything I have done to him.
+                  For example, my cooking suited his taste.
+                  Bet he will miss me a lot?"
+                 -> "he loves everything i have done to him.
+                     for example, my cooking suited his taste.
+                     bet he will miss me a lot?"
+        :param utterance: String
+        :return: String
+        """
         return utterance.lower()
 
     def filterPunct(self, utterance):
+        """
+        Filter punctuation in a string.
+            e.g. "he loves everything i have done to him.
+                  for example, my cooking suited his taste.
+                  bet he will miss me a lot?"
+                 -> "he loves everything i have done to him
+                     for example my cooking suited his taste
+                     bet he will miss me a lot"
+        :param utterance: String
+        :return: String
+        """
         return re.sub(r"[^\w\s]", "", utterance)
 
     def tokenizing(self, utterance):
+        """
+        Turn a string into words or tokens.
+            e.g. "he loves everything i have done to him
+                  for example my cooking suited his taste
+                  bet he will miss me a lot"
+                 -> ["he", "loves", "everything", "i", "have", "done", "to", "him",
+                     "for", "example", "my", "cooking", "suited", "his", "taste",
+                     "bet", "he", "will", "miss", "me", "a", "lot"]
+        :param utterance: String
+        :return: tokens: List
+        """
         return utterance.split()
