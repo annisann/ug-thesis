108c5d4fb4ac3aa0840717953172fbfb505823ef
annisann <ennoza@student.ub.ac.id>
2022-06-04 00:03:31 +0700

    adjust splitData for file as the input

--- DataProcessing.py
+++ DataProcessing.py
@@ -2,6 +2,7 @@ import glob
 import json
 import os
 import re
+import ast
 
 
 class DataProcessing:
@@ -51,17 +52,16 @@ class DataProcessing:
         :param path: transcription file (.txt)
         :return: list of dictionaries
         """
-        self.path = path
-        self.transcriptions = list()
+        transcriptions = list()
 
-        file = open(self.path, 'r').read().splitlines()  # [utt_1, utt_2, .. , utt_n]
+        file = open(path, 'r').read().splitlines()  # [utt_1, utt_2, .. , utt_n]
 
         for f in file:
-            self.transcriptDict = dict(id=self.getSpeaker(f),
-                                       utterance=self.getUtterance(f)
-                                       )
-            self.transcriptions.append(self.transcriptDict)
-        return self.transcriptions
+            transcriptDict = dict(id=self.getSpeaker(f),
+                                  utterance=self.getUtterance(f)
+                                  )
+            transcriptions.append(transcriptDict)
+        return transcriptions
 
     def getVAD(self, path):
         """
@@ -69,21 +69,20 @@ class DataProcessing:
         :param path: EmoEvaluation file (.txt)
         :return: list of dictionaries
         """
-        self.path = path
-        self.emos = list()
+        emos = list()
 
-        file = open(self.path, 'r').read().splitlines()
+        file = open(path, 'r').read().splitlines()
         file = [f for f in filter(lambda x: x.startswith('['), file)]
 
         for f in file:
-            self.emoDict = dict(id=self.getSpeaker(f),
-                                v=float(f.split('\t')[3][1:-1].split(',')[0]),
-                                a=float(f.split('\t')[3][1:-1].split(',')[1]),
-                                d=float(f.split('\t')[3][1:-1].split(',')[2])
-                                )
-            self.emos.append(self.emoDict)
+            emoDict = dict(id=self.getSpeaker(f),
+                           v=float(f.split('\t')[3][1:-1].split(',')[0]),
+                           a=float(f.split('\t')[3][1:-1].split(',')[1]),
+                           d=float(f.split('\t')[3][1:-1].split(',')[2])
+                           )
+            emos.append(emoDict)
 
-        return self.emos
+        return emos
 
     def fixUtterance(self, listOfDict):
         """
@@ -94,7 +93,7 @@ class DataProcessing:
         counter = 1
         utt_per_speaker = []
 
-        # COUNT DUPLICATE CONSECUTIVE UTTERANCES
+        # COUNT CONSECUTIVE DUPLICATE SPEAKERS
         for i in range(len(listOfDict) - 1):
             currSpeaker = listOfDict[i]['id'][0]
             nextSpeaker = listOfDict[i+1]['id'][0]
@@ -106,7 +105,7 @@ class DataProcessing:
                 counter = 1
         utt_per_speaker.append(counter)
 
-        # MERGE DUPLICATE CONS. UTTERANCES INTO ONE
+        # MERGE DUPLICATE SPEAKER'S UTTERANCE
         index = 0
         utterances = []
         for n in utt_per_speaker:
@@ -127,36 +126,34 @@ class DataProcessing:
         :param path: path of preprocessed scripts
         :return:
         """
-        # for every scripts in preprocessed_data
-            # open file, isi file jadi list
-            # count length of [] scripts
-            # index[] -> index of 0:2, 2:4, ... -> (0, len_scripts, n)
-            # while i < len_scripts
-                # while counter < len(index)-1
-                    # n_utterances -> scripts[i][idx[counter]:idx[counter+1]]
-                    # open new file -> nama baru script_i_counter.txt
-                        # write file of [{},..,{}] ke path baru
-                    # counter+=1
-                # i+=1
-
         FOLDER_NAME = f'{str(n)}-utterances'
 
         if not os.path.exists(FOLDER_NAME):
-            os.makedirs(FOLDER_NAME)
+            os.mkdir(FOLDER_NAME)
 
-        len_data = len(os.listdir(path))  # 151
-        idx = [i for i in range(0, len_data, n)]  # ???? len isi scriptnya yaaa
-
-        i = 0
-        while i < len_data:
-            counter = 0
-            while counter < len(idx)-1:
-                # with open('{}/script{}_{}.txt'.format(PATH, str(i), str(counter)), 'w') as file:
-                with open(f'{FOLDER_NAME}/script_{str(i)}_{str(counter)}.txt', 'w') as file:
-                    # n_utterances = scripts[i][idx[counter]:idx[counter + 1]] #?????? ganti kyny
-                    if len(n_utterances) == n:
+        # for every scripts in preprocessed_data
+        for script in os.listdir(path):
+            print(f'{path}/{script}')
+            # open file, isi file jadi list
+            f = open(f'{path}/{script}').read()
+            f = ast.literal_eval(f)
+            # count length of [] scripts
+            script_len = len(f)
+            print(script_len)
+            # index[] -> index of 0:2, 2:4, ... -> (0, len_scripts, n)
+            idx = [i for i in range(0, script_len+1, n)]
+            print(len(idx), idx)
+            # while i < len_scripts
+            i = 0
+            while i < script_len:
+                counter = 0
+                # while counter < len(index)-1
+                while counter < len(idx)-1:
+                    # n_utterances -> scripts[i][idx[counter]:idx[counter+1]]
+                    n_utterances = f[idx[counter]:idx[counter+1]]
+                    # open new file -> nama baru .txt
+                    with open(f'{FOLDER_NAME}/{script}_{counter}.txt', 'w') as file:
+                        # write file of [{},..,{}] ke path baru
                         file.write(json.dumps(n_utterances))
-                    else:
-                        break
-                counter += 1
-            i += 1
+                    counter+=1
+                i+=1
