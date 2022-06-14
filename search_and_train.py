a22b97e392f0b05f34682c3f10c59b42cf97b68d
annisann <ennoza@student.ub.ac.id>
2022-06-14 21:53:34 +0700

    add new args, call functions from utils.py, and delete unnecessary line

--- search_and_train.py
+++ search_and_train.py
@@ -11,59 +11,37 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
-from torch_lr_finder import LRFinder
-
-from skorch import NeuralNet # atau classifier?
-from skorch import NeuralNetRegressor
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_squared_error, make_scorer
 
 pretrain = PretrainedEmbeddings()
-# config = {
-#     'n_utterances': 2,
-#     'max_seq_length': 10,
-#     'dim': 200,
-#     'n_epoch': 20,
-#     'with_attention': False,
-#
-#     # ENCODER
-#     'pretrained_embeddings': None,
-#     'freeze_embeddings': True,
-#     'en_hidden_size': 16,
-#
-#     # EMO RECOG
-#     'num_layers': 1,
-#     'hidden_size': 16,
-#     'lr': 0.01
-#     }
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--epochs', dest='n_epochs', default=20, type=int,
                     help='number of epochs to train network')
-parser.add_argument('-lr', '--learning-rate', dest='lr', default=0.01, type=float,
-                    help="optimizer's learning rate")
 parser.add_argument('-utt', '--n-utterances', dest='n_utterances', default=2, type=int,
                     help='number of utterances')
+parser.add_argument('-opt', '--optimizer', dest='optimizer', default='adam', type=str,
+                    help='type of optimizer')
+parser.add_argument('-lr', '--learning-rate', dest='lr', default=0.05, type=float,
+                    help="optimizer's learning rate")
+parser.add_argument('-bs', '--batch-size', dest='batch_size', default=256, type=int,
+                    help='batch size of the data')
 parser.add_argument('-dim', '--embed-dim', dest='dim', default=50, type=int,
                     help='embedding dimension size')
-parser.add_argument('-l', '--n-layers', dest='num_layers', default=1, type=int,
-                    help='number of bilstm layers')
-parser.add_argument('-s', '--seq-len', dest='max_seq_len', default=10, type=int,
+parser.add_argument('-s', '--seq-len', dest='max_seq_len', default=20, type=int,
                     help='sequence length for encoder')
 parser.add_argument('-eh', '--en-hidden', dest='en_hidden_size', default=16, type=int,
                     help='encoder hidden size')
+parser.add_argument('-l', '--n-layers', dest='num_layers', default=1, type=int,
+                    help='number of bilstm layers')
+
 parser.add_argument('-hs', '--hidden', dest='hidden_size', default=16, type=int,
                     help='hidden size')
-parser.add_argument('-bs', '--batch-size', dest='batch_size', default=16, type=int,
-                    help='batch size of the data')
-parser.add_argument('--attention', dest='with_attention', action='store_true')
+
+parser.add_argument('--attention', dest='with_attention', action='store_true', default=True)
+
+parser.add_argument('--lr-scheduler', dest='lr_scheduler', action='store_true')
 parser.add_argument('--early-stop', dest='early_stopping', action='store_true')
 config = vars(parser.parse_args())
-
-def count_time(elapsed_time):
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
 
 def prepare_data(dataset, n_utterances):
     """
@@ -85,7 +63,6 @@ def prepare_data(dataset, n_utterances):
             seq.append(data)
     return seq
 
-
 def train_and_evaluate(config, train_data, val_data):
 
     run_dir = create_run_dir()
@@ -101,15 +78,21 @@ def train_and_evaluate(config, train_data, val_data):
     total_params = sum(p.numel() for p in model.parameters())
     print(f'Model has {total_params:,} trainable parameters.')
 
-
     # loss function
     criterion = nn.MSELoss()
     # optimizer
-    optimizer = optim.Adam(model.parameters(), lr=config['lr'])
-    # lr_finder = LRFinder(model, optimizer, criterion)
-    # lr_finder.range_test(train_data, val_loader=val_data, end_lr=1, num_iter=50, step_mode="linear")
-    # lr_finder.plot(log_lr=False)
-    # lr_finder.reset()
+    if config['optimizer'] == 'adam':
+        optimizer = optim.Adam(model.parameters(), lr=config['lr'])
+    if config['optimizer'] == 'adamw':
+        optimizer = optim.AdamW(model.parameters(), lr=config['lr'])
+
+    # see init weights
+    with open(f"{run_dir}/weights.txt", "w") as f:
+        f.write(f"========== INIT WEIGHTS ==========\n")
+        for name, param in model.named_parameters():
+            # print(f"{name}\n{param}")
+            f.write(f"{name}\n{param}\n")
+
 
     # best_val_loss = float('inf')
     train_loss, val_loss = [], []
@@ -122,11 +105,27 @@ def train_and_evaluate(config, train_data, val_data):
 
         end_time = time.time()
 
-        epoch_mins, epoch_secs = count_time(end_time-start_time)
+        epoch_hours, epoch_mins, epoch_secs = count_time(end_time-start_time)
 
         train_loss.append(epoch_train_loss)
         val_loss.append(epoch_val_loss)
         train_time.append(end_time-start_time)
+
+        # save weights
+        with open(f"{run_dir}/weights.txt", "a") as f:
+            f.write(f"\n========== EPOCH {epoch+1} ==========\n")
+            for name, param in model.named_parameters():
+                # print(f"{name}\n{param}")
+                f.write(f"{name}\n{param}\n")
+
+
+        if config['lr_scheduler']:
+            lr_scheduler = LRScheduler(optimizer)
+            lr_scheduler(epoch_val_loss)
+            current_lr = get_lr(optimizer)
+            print(current_lr)
+            with open(f"{run_dir}/learning_rate.txt", "a") as f:
+                f.write(f"Epoch {epoch}: {current_lr}\n")
 
         if config['early_stopping']:
             early_stopping = EarlyStop()
@@ -135,16 +134,22 @@ def train_and_evaluate(config, train_data, val_data):
                 break
 
         print('-' * 100)
-        print(f'[INFO] Epoch {epoch + 1}/{config["n_epochs"]} | Time: {epoch_mins}m {epoch_secs}s')
+        print(f'[INFO] Epoch {epoch + 1}/{config["n_epochs"]} | Time: {epoch_hours}h {epoch_mins}m {epoch_secs}s')
         print(f'Train loss: {epoch_train_loss}')
         print(f'Val loss: {epoch_val_loss}')
 
     # Total training time
     total_train_time = sum(train_time)
-    train_mins, train_secs = count_time(total_train_time)
-    print(f'[INFO] Total Training Time: {train_mins}m {train_secs}s')
+    train_hours, train_mins, train_secs = count_time(total_train_time)
+    print(f'[INFO] Total Training Time: {train_hours}h {train_mins}m {train_secs}s')
 
     # Save file
+    save_train_time(train_time,
+                    f'{run_dir}/train_time.txt')
+
+    save_losses(train_loss, val_loss,
+                f'{run_dir}/loss.txt')
+
     save_plots(train_loss, val_loss,
                f'{run_dir}/loss.png')
 
@@ -163,18 +168,15 @@ if __name__ == '__main__':
 
     train_seq = prepare_data(traindf, config['n_utterances'])
     val_seq = prepare_data(valdf, config['n_utterances'])
-    test_seq = prepare_data(testdf, config['n_utterances'])
 
     train_data = NUtterancesDataset(train_seq)
     val_data = NUtterancesDataset(val_seq)
-    test_data = NUtterancesDataset(test_seq)
 
     train_loader = DataLoader(train_data, batch_size=config['batch_size'], shuffle=True)
     val_loader = DataLoader(val_data, batch_size=config['batch_size'], shuffle=True)
-    test_loader = DataLoader(test_data, batch_size=config['batch_size'], shuffle=True)
 
     print('Train and evaluate')
     train_and_evaluate(config, train_seq, val_seq)
-    # train_and_evaluate(config, train_loader, val_loader)
-    # print('Search')
-    # run_search(val_data)
+
+    # print('Test')
+    # test(model, data, criterion)
