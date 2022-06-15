5baeafb2ca0f23f8e07f2344bd25b59acbfcf42a
annisann <ennoza@student.ub.ac.id>
2022-06-15 15:00:28 +0700

    adjust new parameter from args

--- Model.py
+++ Model.py
@@ -16,17 +16,20 @@ class UtteranceEncoder(nn.Module):
 
     def __init__(self,
                  pretrained_embeddings,
-                 freeze_embeddings=True,
-                 num_layers=None,
-                 hidden_size=None):
+                 freeze_embeddings,
+                 num_layers,
+                 hidden_size,
+                 dropout_rate):
         """
         :param encoded_input: list of padded utterance (encoded)
         """
         super(UtteranceEncoder, self).__init__()
 
         embeddings_dim = pretrained_embeddings.shape[1]
+        print(f"Encoder dim {embeddings_dim}")
         self.num_layers = num_layers
         self.hidden_size = hidden_size
+        self.dropout = nn.Dropout(p=dropout_rate)
 
         self.embedding = nn.Embedding.from_pretrained(torch.Tensor(pretrained_embeddings),
                                                       freeze=freeze_embeddings)
@@ -51,6 +54,7 @@ class UtteranceEncoder(nn.Module):
         input = torch.Tensor(seq_token).long()  # torch.Size([512])
 
         embed = self.embedding(input)  # batch_size, seq_len, input_size
+        embed = self.dropout(embed)
         embed = embed.unsqueeze(0)
 
         hidden_state = self.init_state(1)  # num_directions, batch_size, hidden_size
@@ -75,24 +79,29 @@ class BiLSTM_Attention(nn.Module):
         super(BiLSTM_Attention, self).__init__()
 
         self.en_hidden_size = config['en_hidden_size']
+        en_n_layer = config['en_n_layer']
+        embedding_dropout_rate = config['embedding_dropout_rate']
         self.hidden_size = config['hidden_size']
         self.num_layers = config['num_layers']
         self.with_attention = config['with_attention']
+
+
         output_size = 3
+        # self.embedding_size = pretrained_embeddings.shape[1]
 
         self.encoder = UtteranceEncoder(pretrained_embeddings=pretrained_embeddings,
                                         freeze_embeddings=True,
-                                        num_layers=self.num_layers,
-                                        hidden_size=self.en_hidden_size
+                                        num_layers=en_n_layer,
+                                        hidden_size=self.en_hidden_size,
+                                        dropout_rate=embedding_dropout_rate
                                         )
 
         self.bilstm = nn.LSTM(input_size=2*self.en_hidden_size,
                               hidden_size=self.hidden_size,
                               num_layers=self.num_layers,
                               bidirectional=True,
-                              batch_first=True
+                              batch_first=True,
                               )
-        self.dropout = nn.Dropout(0.2)
 
         self.fc_attention = nn.Linear(self.hidden_size*2, self.hidden_size*2,
                                       bias=False)
@@ -109,7 +118,7 @@ class BiLSTM_Attention(nn.Module):
                 torch.zeros(2 * self.num_layers, batch_size, self.hidden_size))
 
     def forward(self, inputs):
-        inputs = torch.Tensor(inputs)  # seq_len, hidden_size
+        inputs = torch.Tensor(inputs) # seq_len, hidden_size
 
         # 2x karena output encoder adalah 2*seq_len
         encoder_out = torch.empty(size=(inputs.size()[0], 2*self.en_hidden_size))  # seq_len, hidden_size
@@ -138,8 +147,8 @@ class BiLSTM_Attention(nn.Module):
             out = self.regression(context_vector).flatten()
             return out
         else:
-            output = self.dropout(output)
-            print(output)
-            # output = output.squeeze(0)[-1]
+            # output = self.dropout(output)
+            # print(output)
+            output = output.squeeze(0)[-1]
             out = self.regression(output).flatten()
             return out
