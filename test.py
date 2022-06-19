9fd2025ca98abfdf921cb2c65647e96753f6b6fb
annisann <ennoza@student.ub.ac.id>
2022-06-19 14:15:20 +0700

    remove prepare_data and add some explanation comments

--- test.py
+++ test.py
@@ -13,31 +13,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--n-dir', dest='n', type=int)
 arg = vars(parser.parse_args())
 
-arg['n'] = 27
-
-
-def prepare_data(dataset, n_utterances):
-    """
-    :param dataset: type of dataset
-    :param n_utterances: num of utterances for input
-    :return: tuple of [seq_token_i, ..., seq_token_n], mean(V), mean(A), mean(D)
-    """
-    seq = []
-
-    for i in range(len(dataset)):
-        if i % n_utterances == 0:
-            data = tuple(
-                (np.array(list(dataset.seq[i:i + n_utterances])),
-                 torch.from_numpy(np.array(sum(list(dataset.v[i:i + n_utterances])) / n_utterances, dtype=np.float32)),
-                 torch.from_numpy(np.array(sum(list(dataset.a[i:i + n_utterances])) / n_utterances, dtype=np.float32)),
-                 torch.from_numpy(np.array(sum(list(dataset.d[i:i + n_utterances])) / n_utterances, dtype=np.float32))
-                 )
-            )
-            seq.append(data)
-    return seq
-
-# load model, isi paramnya dari .yaml
-with open(f'outputs/run_{n}/hyperparameters.yml') as f:
+# load hyperparameters from yml file.
+with open(f'outputs/run_{arg["n"]}/hyperparameters.yml') as f:
     hyperparam_tuples = re.findall('(\w+): (.+)', f.read(), re.MULTILINE)
     hyperparam_dict = {k: v for k, v in hyperparam_tuples}
 
@@ -50,27 +27,28 @@ for k, v in hyperparam_dict.items():
     elif k in is_float_val:
         hyperparam_dict[k] = float(v)
 
+# load pretrained embeddings
 pretrain = PretrainedEmbeddings()
-
 vocab, embeddings = pretrain.load_from_file(hyperparam_dict['dim'])
-
 dataset = ConvEmoRecogDataset(utterance_num=hyperparam_dict['n_utterances'],
                               vocab=vocab,
                               max_seq_length=hyperparam_dict['max_seq_len'])
 traindf, valdf, testdf = dataset.load_dataset()
 test_seq = prepare_data(testdf, hyperparam_dict['n_utterances'])
 
+# create new dir
 test_dir = create_test_dir()
 
 # load weight from path
 model = BiLSTM_Attention(hyperparam_dict, embeddings)
-model.load_state_dict(torch.load(f'outputs/run_{n}/state_dict.pt')) # kalo mau diautomate, ganti ini.. lot of work? nyampe gak ya lol
-
+model.load_state_dict(torch.load(f'outputs/run_{arg["n"]}/state_dict.pt')) # kalo mau diautomate, ganti ini.. lot of work? nyampe gak ya lol
+# loss function
 criterion = nn.MSELoss()
 
 print(f"i\t\tActual VAD\t\t\tPredicted VAD\tLoss")
 print("-"*50)
 
+# write test result init
 with open(f'{test_dir}/out.txt', "w") as savefile:
     savefile.write(f"i\t\tActual VAD\t\t\tPredicted VAD\tLoss\n")
 
@@ -86,15 +64,19 @@ for i, input in enumerate(test_seq):
         loss = loss_v + loss_a + loss_d
         total_loss += loss
 
+        # display true vad value, predicted vad value, and MSE loss
         print(f"{i}\t({v_act:.2f}, {a_act:.2f}, {d_act:.2f})\t({v_pred:.2f}, {a_pred:.2f}, {d_pred:.2f})\t{loss:.2f}")
 
+    # write true vad value, predicted vad value, and MSE loss
     with open(f'{test_dir}/out.txt', "a") as savefile:
         savefile.write(f"{i}\t"
                        f"({v_act:.2f}, {a_act:.2f}, {d_act:.2f})\t"
                        f"({v_pred:.2f}, {a_pred:.2f}, {d_pred:.2f})\t"
                        f"{loss:.2f}\n")
 
+# write the average loss of test data
 with open(f'{test_dir}/out.txt', "a") as savefile:
     savefile.write(f"\nTotal Loss: {total_loss/len(test_seq)}")
 
+# show the average loss of test data
 print(total_loss/len(test_seq))
